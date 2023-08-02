9b48df36769a7e2c7d73c94d486e41db83540b2a
koyotex <david242455@gmail.com>
2023-08-02 15:53:57 -0400

    cambiando puerto

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { router } from "./routes/productRoutes.js";
 import cors from 'cors'
 
 const app = express()
-const port = 3000
+const port = 80
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
