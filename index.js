8db5e2b668924cd1b7f58f31926d0019ceb589df
AlejandroCarpioH <david242455@gmail.com>
2023-09-02 10:30:57 -0400

    test token

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import validateJson from "./middleware/validateJson.js";
 import hasToken from "./middleware/hasToken.js";
-import generateToken from "./middleware/generateToken.js";
+
 dotenv.config()
 
 const app = express()
@@ -14,8 +14,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(validateJson);
-app.use('/api/v1/generateToken', generateToken)
-app.use('/', hasToken, router)
+
+app.use('/', router)
 
 app.listen(port, () => {
     console.log("server iniciado")
