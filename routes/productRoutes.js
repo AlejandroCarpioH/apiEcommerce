9953f112355abfddad71b20792b9f86dbe793865
AlejandroCarpioH <david242455@gmail.com>
2024-05-26 03:41:24 -0400

    add new text to text

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -15,7 +15,7 @@ import {
 const router = express.Router()
 
 router.get('/api/v1/test', hasToken, (req, res) => {
-    res.send("autorizado nuevo 4")
+    res.send("this is a test for testing github actions :)")
 })
 
 
